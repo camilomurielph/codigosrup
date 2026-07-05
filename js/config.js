e3f067d6b6d6346ccc73d2cc2e41652788c0c1e7
camilomurielph <149724435+camilomurielph@users.noreply.github.com>
2026-07-04 23:13:25 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// js/config.js
+export const APPWRITE_CONFIG = {
+    endpoint: 'https://nyc.cloud.appwrite.io/v1', // Si es self-hosted, cámbialo
+    projectId: '6a49d764000556cc596d', // Cópialo de Appwrite Console
+    databaseId: 'rup_db',       // El ID de la base de datos que creaste
+    collectionId: 'codigos'     // El ID de la colección que creaste
+};
